dfe16989e5651aa9bfa5c8aaa933e366e52ae3a2
sanjayaharshana <sanjayaharasana@gmail.com>
2024-08-11 11:11:22 +0530

    Templte Relationshop

--- app/Models/Templetes.php
+++ app/Models/Templetes.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\TempleteCategories;
+use App\Models\User;
 
 class Templetes extends Model
 {
     use HasFactory;
+
+
+    public function category()
+    {
+        return $this->belongsTo(TempleteCategories::class, 'category_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
