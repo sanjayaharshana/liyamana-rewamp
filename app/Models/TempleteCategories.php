6ec37f14cf075f9014a1357f80840bd14d8392f6
sanjayaharshana <sanjayaharasana@gmail.com>
2024-08-11 11:11:14 +0530

    Templetes Ccagorsis

--- app/Models/TempleteCategories.php
+++ app/Models/TempleteCategories.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class TempleteCategories extends Model
 {
     use HasFactory;
+
+    public function templates()
+    {
+        return $this->hasMany(Templetes::class, 'category_id');
+    }
 }
