b494b89af7a210e6fea67e675132aea54d91b6e1
sanjayaharshana <sanjayaharasana@gmail.com>
2024-08-13 01:40:19 +0530

    make Place Controller Services

--- app/Http/Controllers/Frontend/MarketPlaceController.php
+++ app/Http/Controllers/Frontend/MarketPlaceController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class MarketPlaceController extends Controller
 {
@@ -15,5 +16,37 @@ class MarketPlaceController extends Controller
     public function show($slug)
     {
         return view('landing.market-place-single');
+    }
+
+    public function jsonSetting()
+    {
+        $path = public_path('editor_json.json'); // Adjust the path as needed
+        $json = File::get($path);
+        $data = json_decode($json, true);
+
+        return response()->json($data);
+    }
+
+    public function jsonProducts()
+    {
+        $outArray = [
+          'status' => true,
+          'products' => [
+              [
+                  'id' => 1,
+                  'name' => 'Sport Tees',
+                  'price' => 310,
+                  'category' => 'clothes',
+                  'currency' => 'USD',
+                  'image' => url('landing_pages/head.jpg'),
+                  'sub_images' => [
+                      url('landing_pages/head.jpg'),
+                      url('landing_pages/head1.jpg'),
+                  ]
+              ]
+          ],
+          'message' => 'Products loaded successfully.'
+        ];
+        return response()->json($outArray);
     }
 }
