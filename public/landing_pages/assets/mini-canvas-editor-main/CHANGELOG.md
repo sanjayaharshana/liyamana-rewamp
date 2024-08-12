3dfae7c3d2249abcafe78aa4050915f545f20b58
sanjayaharshana <sanjayaharasana@gmail.com>
2024-08-13 02:30:55 +0530

    Assets Servies

---
+++ public/landing_pages/assets/mini-canvas-editor-main/CHANGELOG.md
@@ -0,0 +1,15 @@
+## 0.3.0
+
+This version changed the license to MIT.
+
+## 0.2.0
+
+This version improves support for UMD bundles.
+
+## 0.1.1
+
+Add `stroke` and `strokeWidth` fields to the properties editor for the textbox.
+
+## 0.1.0
+
+Initial release. 🚀
---
+++ public/landing_pages/assets/mini-canvas-editor-main/demos/webpack-app/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+function bundle(name) {
+	return {
+		entry: `./src/${name}.ts`,
+		cache: false,
+		module: {
+			rules: [
+				{
+					test: /\.ts$/,
+					use: 'ts-loader',
+					exclude: /node_modules/,
+				},
+				{
+					test: /\.css$/i,
+					use: ['style-loader', 'css-loader'],
+				},
+			],
+		},
+		resolve: {
+			extensions: ['.tsx', '.ts', '.js'],
+		},
+		output: {
+			filename: `${name}.js`,
+			path: path.resolve(__dirname, 'public/builds'),
+		}
+	}
+}
+
+module.exports = [
+	bundle('crop'),
+	bundle('inpainting-mask'),
+	bundle('template-creator'),
+];
