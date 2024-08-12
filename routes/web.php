a94bbd52bdc8f416ece67d848150f243dfbde61f
sanjayaharshana <sanjayaharasana@gmail.com>
2024-08-13 01:39:37 +0530

    Packing Services

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',[\App\Http\Controllers\Frontend\HomeController::class,'index'])->name('landing.home');
 Route::get('market-place',[\App\Http\Controllers\Frontend\MarketPlaceController::class,'index'])->name('landing.home');
 Route::get('market-place/{slug}',[\App\Http\Controllers\Frontend\MarketPlaceController::class,'show'])->name('landing.home');
+Route::get('market-place/inc/json/settings.json',[\App\Http\Controllers\Frontend\MarketPlaceController::class,'jsonSetting'])->name('landing.home');
+Route::get('market-place/dummy_data/products/load_products.json',[\App\Http\Controllers\Frontend\MarketPlaceController::class,'jsonProducts'])->name('landing.home');
