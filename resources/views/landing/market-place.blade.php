bc6e14c2722bcaef429a7e447402113d2d8f5846
sanjayaharshana <sanjayaharasana@gmail.com>
2024-08-13 09:22:32 +0530

    Market Place Blades

--- resources/views/landing/market-place.blade.php
+++ resources/views/landing/market-place.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
 
+
+
     <div style="margin-top: 100px"></div>
     <div class="container">
         <div class="row">
@@ -10,21 +12,39 @@
 
             </div>
             <div class="col-md-8">
-                <div class="row">
-                    <div class="col-md-4">
-                        <a href="{{url('market-place/apple0slug')}}">
-                            <div class="card" style="margin-bottom: 10px;">
-                                <div style="background: url('{{url('landing_pages/assets/img/portfolio/portfolio-1.jpg')}}');height: 100px;background-size: cover;background-repeat: no-repeat;"></div>
-                                <div class="card-body">
-                                    <h4>Product Item 1</h4>
-                                    <p style="font-size: 13px;">Rediscover the joy of sending and receiving letters with Liyamana, the modern postal platform that bridges the gap between tradition and technology </p>
-                                </div>
-                            </div>
-                        </a>
+                <div style="height:100px;background: url('https://blucactus.blue/wp-content/uploads/2020/09/Blucactus-What-is-e-commerce-cover-page-1.jpg')">
 
-                    </div>
                 </div>
-
+                <div class="row">
+                    @foreach($templates as $templeteItem)
+                        <div class="col-md-4">
+                            <a href="{{url('market-place/'.$templeteItem->slug)}}">
+                                <div class="card" style="margin-bottom: 10px;">
+                                    <div style="background: url('{{url('storage/'.$templeteItem->images)}}');height: 200px;background-size: cover;background-repeat: no-repeat;"></div>
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <h3 style="font-size: 20px;text-align: center;">Product Item 1</h3>
+                                            <p style="text-align: center;font-size: 11px;margin-bottom: 9px;">With envolope cover and Pipin paper letter</p>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                               <div style="background: #9f202c;color: white;text-align: center;border-radius: 7px;">Letter</div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                LKR {{number_format($templeteItem->price,2)}}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card" style="margin-bottom: 10px;">
+                    HARRY
+                </div>
             </div>
         </div>
 
