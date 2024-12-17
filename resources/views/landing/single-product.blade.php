7c9814924043fb3edaaef4d9988ae1cc7a1a4372
sanjayaharshana <sanjayaharasana@gmail.com>
2024-12-17 11:04:04 +0530

    Single Product Blade Updates

--- resources/views/landing/single-product.blade.php
+++ resources/views/landing/single-product.blade.php
@@ -8,11 +8,11 @@
 
     <div class="container">
        <div class="row">
-           <div class="col-md-6">
+           <div class="col-md-5">
                @include('landing.product_page.product_slider')
 
            </div>
-           <div class="col-md-6">
+           <div class="col-md-4">
 
                             @if ($errors->any())
                    <div style="background: #992121;color: white;padding: 5px;margin-bottom: 10px;padding-bottom: 1px;padding-top: 15px;border-radius: 10px;">
@@ -24,7 +24,7 @@
                    </div>
                @endif
                <h2>{{$template->name}}</h2>
-               <p>{{$template->description}}</p>
+               <p style="font-size: 14px;">{{$template->description}}</p>
 
                <p style="background: url('https://www.shutterstock.com/image-photo/wood-texture-natural-plywood-background-600nw-1901761825.jpg');color: white;padding: 10px;width: 170px;border-radius: 10px;text-align: center;font-size: 20px;background-size: contain;">
                    LKR : {{number_format($template->price,2)}}
@@ -37,6 +37,59 @@
 
 
                <button class="btn btn-primary" style="background: maroon;border-style: none"><i class="bi bi-pen" style="margin-right: 10px"></i> Edit This Template</button>
+           </div>
+
+           <div class="col-md-3">
+               <div style="background: #f3f3f3;">
+                   <div class="row">
+                       <div class="col-md-12">
+                           <div style="padding: 10px;">
+                               <div style="font-size: 11px;">Sending Options</div>
+                               <div style="margin-top: 10px;background: #eaeaea;padding-left: 9px;padding-right: 9px;border-radius: 4px;padding-bottom: 5px;font-size: 14px;">
+                                   <i class="bi bi-envelope-open" style="font-size: 25px;"></i>
+                                   <span style="margin-left: 10px;">Send via Email</span>
+                               </div>
+                           </div>
+
+                           <div style="padding: 10px;">
+                               <div style="background: #eaeaea;padding-left: 9px;padding-right: 9px;border-radius: 4px;padding-bottom: 5px;font-size: 14px;">
+                                   <i class="bi bi-map" style="font-size: 25px;"></i>
+                                   <span style="margin-left: 10px;">Share this template</span>
+                               </div>
+                           </div>
+
+                           <div style="padding: 10px;">
+                               <div style="background: #eaeaea;padding-left: 9px;padding-right: 9px;border-radius: 4px;padding-bottom: 5px;font-size: 14px;">
+                                   <i class="bi bi-shop" style="font-size: 25px;"></i>
+                                   <span style="margin-left: 10px;">Help and Support</span>
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+
+               <br>
+               <div style="background: #f3f3f3;">
+                   <div class="row">
+                       <div class="col-md-12">
+                           <div style="padding: 10px;">
+                               <div style="font-size: 11px;">More Information</div>
+                               <div style="margin-top: 10px;background: #eaeaea;padding-left: 9px;padding-right: 9px;border-radius: 4px;padding-bottom: 5px;font-size: 14px;">
+                                   <i class="bi bi-card-heading" style="font-size: 25px;"></i>
+                                   <span style="margin-left: 10px;">Default Letter Type</span>
+                                   <div style="margin-left: 40px;font-size: 11px">A4 (Offset 291)</div>
+                               </div>
+                           </div>
+
+                           <div style="padding: 10px;">
+                               <div style="background: #eaeaea;padding-left: 9px;padding-right: 9px;border-radius: 4px;padding-bottom: 5px;font-size: 14px;">
+                                   <i class="bi bi-printer" style="font-size: 25px;"></i>
+                                   <span style="margin-left: 10px;">Inkject (Canon e202)</span>
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+               </div>
            </div>
        </div>
     </div>
@@ -180,6 +233,10 @@
             </div>
         </div>
     </div>
+
+
+
+
 @endsection
 
 @push('footer-js')
