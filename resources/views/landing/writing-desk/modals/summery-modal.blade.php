e2c71b41587f2d7ef0627c82cb1a9cf6df4acc5e
sanjayaharshana <sanjayaharasana@gmail.com>
2025-01-20 13:32:01 +0530

    Summry Model Blades

--- resources/views/landing/writing-desk/modals/summery-modal.blade.php
+++ resources/views/landing/writing-desk/modals/summery-modal.blade.php
@@ -1,3 +1,20 @@
+<style>
+    .nav-link{
+        color: white;
+    }
+
+    .nav-pills .nav-link.active, .nav-pills .show>.nav-link {
+        color: white;
+        background-color: #570707;
+    }
+
+    .nav-link:focus, .nav-link:hover{
+        text-decoration: none;
+        background-color: rgba(87, 7, 7, 0);
+        color: white;
+    }
+</style>
+
 <div class="modal fade" id="finalizemodal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-xl" style="">
         <div class="modal-content" style="">
@@ -5,13 +22,12 @@
                 <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
                     <li class="nav-item" role="presentation">
                         <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true" style="
-    background: #570707;
 ">Envelope</button>
                     </li>
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false" style="
+                        <button  class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false" style="
     color: white;
-" tabindex="-1">Attachment</button>
+" tabindex="-1">Print Details</button>
                     </li>
                     <li class="nav-item" role="presentation">
                         <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false" tabindex="-1" style="
